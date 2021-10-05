6eaa9aed4423c5bb2872a630cdfd79dcc0ebbfbb
kangpeiqin <1060647301@qq.com>
2021-10-05 09:58:20 +0800

    Oauth 2.0 demo

--- spring-boot-module/oauth2ClientDemo/src/main/java/com/example/oauthServer/Oauth2ClientApplication.java
+++ spring-boot-module/oauth2ClientDemo/src/main/java/com/example/oauthServer/Oauth2ClientApplication.java
@@ -2,8 +2,10 @@ package com.example.oauthServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
 @SpringBootApplication
+@EnableOAuth2Client
 public class Oauth2ClientApplication {
 
     public static void main(String[] args) {
--- spring-cloud/oauth2ServerDemo/src/main/java/com/example/oauthServer/config/AuthorizationServerConfig.java
+++ spring-cloud/oauth2ServerDemo/src/main/java/com/example/oauthServer/config/AuthorizationServerConfig.java
@@ -1,23 +1,12 @@
 package com.example.oauthServer.config;
 
-import com.example.oauthServer.service.UserDetailService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 /**
  * 创建认证服务器：作为统一令牌发放并校验的地方
+ * 为应用增加了一系列的REST端点，从而提供一个标准的OAuth 2.0的实现。
  *
  * @author kpq
  * @since 1.0.0
