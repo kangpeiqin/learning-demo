87f2ce9ffc85c38a48242f01e4951e58d648a22a
kangpeiqin <peiqinkang@163.com>
2021-10-08 16:36:11 +0800

    feat：redis延时队列

---
+++ spring-boot-module/oauth2ClientDemo/src/main/java/com/example/client/delayQueue/container/ReadyQueue.java
@@ -0,0 +1,13 @@
+package com.example.client.delayQueue.container;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author KPQ
+ * @date 2021/10/8
+ */
+@Component
+@Slf4j
+public class ReadyQueue {
+}
