5afb5dae49e43cc3e455711ab6a5919400c31568
kangpeiqin <peiqinkang@163.com>
2021-11-18 20:21:59 +0800

    Spring Security Demo

--- spring-cloud/mini_system/src/main/java/com/example/demo/rest/TestController.java
+++ spring-cloud/mini_system/src/main/java/com/example/demo/rest/TestController.java
@@ -1,9 +1,13 @@
 package com.example.demo.rest;
 
+import cn.hutool.http.HttpRequest;
 import com.example.demo.security.util.ResultUtil;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 /**
  * @author KPQ
@@ -18,7 +22,17 @@ public class TestController {
     }
 
     @GetMapping("/test")
-    public ResultUtil<String> unauthorized(@RequestParam("code") String code) {
-        return ResultUtil.success(code);
+    public ResultUtil<String> unauthorized(@RequestParam("code") String code) throws UnsupportedEncodingException {
+        final String url = "http://127.0.0.1:8080/oauth/token";
+        String res = HttpRequest.post(url)
+                .body(URLEncoder.encode("grant_type", "utf-8"), URLEncoder.encode("authorization_code", "utf-8"))
+                .body("code", URLEncoder.encode(code, "utf-8"))
+                .body("scope", "READ")
+                .body("redirect_uri", "http://127.0.0.1:8089/test")
+                .body("clientId", "oauth2")
+                .header("Authorization", "Basic b2F1dGgyOm9hdXRoMg==")
+                .header("Content-Type", "application/x-www-form-urlencoded")
+                .execute().body();
+        return ResultUtil.success(res);
     }
 }
